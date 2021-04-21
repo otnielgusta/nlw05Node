44e33bf8d2e03c61791419f2ab1e26a99ae5bccd
Otniel Silva <otnielsilvag4@gmail.com>
2021-04-21 00:56:08 -0300

    configurado o typeorm, migrations, entities,  repositories

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,12 @@
 import express, { response } from "express";
+import "./database";
+import {routes} from "./routes";
 
 const app = express();
 
-app.get("/", (request, response )=>{
-    return response.json({
-      message: "otniel lindo"
-    })
-})
-app.post("/users", (request, response)=>{
-  return response.json({
-    message:"Usuario salvo com sucesso"
-  })
-})
+app.use(express.json());
+app.use(routes);
+
+
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
